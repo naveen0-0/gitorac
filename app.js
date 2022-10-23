953c55edbbceaac6e5fc598b2d9dc94b908bfdf4
naveenkumarreddy1910 <naveen.k@mtechzilla.com>
2022-10-23 13:40:41 +0530

    some chnages

---
+++ app.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>app</div>
+  )
+}
+
+export default app
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const app = props => {
+  return (
+    <div>app</div>
+  )
+}
+
+app.propTypes = {}
+
+export default app
